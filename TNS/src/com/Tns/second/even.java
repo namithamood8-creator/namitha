811e4a5f1d51bc33ca9c0e2daf97f14a12399095
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-09-15 09:52:52 +0530

    HII

---
+++ TNS/src/com/Tns/second/even.java
@@ -0,0 +1,14 @@
+package com.Tns.second;
+
+public class even {
+public static void main(String[] args) {
+	int n=10;
+	for(int i=2;i<n;i++){
+		System.out.println("even");
+	}
+		System.out.println("odd");
+	}
+		
+	}
+
+
