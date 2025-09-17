3f389e1cf5264171751f3937a2122d8059b5ac0d
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-09-17 20:31:32 +0530

    nami

--- TNS/src/com/Tns/primenot/primeornot.java
+++ TNS/src/com/Tns/primenot/primeornot.java
@@ -1,7 +1,7 @@
 package com.Tns.primenot;
 import java.util.Scanner;
 
-public class primeornot {
+public class primeornot4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
