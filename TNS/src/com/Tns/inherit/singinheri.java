f44ff18347259cdff9c71c76ff8c3b3df0cca32d
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-09-17 20:02:41 +0530

    nami

--- TNS/src/com/Tns/inherit/singinheri.java
+++ TNS/src/com/Tns/inherit/singinheri.java
@@ -1,24 +1,23 @@
 package com.Tns.inherit;
 
-class singinheri {   // Changed to start with capital letter (Java convention)
+class SingleInheri {
     int a = 10;
-
     public void show() {
         System.out.println("This is class A");
     }
 }
 
-class B extends SingInheri {
+class B extends SingleInheri {
     public void display() {
         System.out.println("This is class B");
     }
 }
 
-public class SingleInheritance {
+public class singinheri {
     public static void main(String[] args) {
-        B obj = new B();   // Object creation
-        obj.display();     // Call method from class B
-        System.out.println(obj.a);  // Access variable from parent class
-        obj.show();        // Call method from parent class
+        B obj = new B();
+        obj.display();
+        System.out.println(obj.a);
+        obj.show();
     }
 }
