2be954de3d3e2d9eb169d6764fc223acf39b3a51
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-09-17 20:05:52 +0530

    aishu

--- TNS/src/com/Tns/Third/armstong.java
+++ TNS/src/com/Tns/Third/armstong.java
@@ -1,27 +1,24 @@
 package com.Tns.Third;
-	import java.util.*;
+import java.util.*;
 
-	public class armstrong {
+public class armst1ong {
 
-		public static void main(String[] args) {
-			Scanner sc= new Scanner(System.in);
-			System.out.println("Enter a number:");
-			int original=sc.nextInt();
-			int n=original;
-			int temp, armstrong=0;
-			while(n>0) {
-				temp=n%10;
-				temp=(int)Math.pow(temp,3);
-				armstrong += temp;
-				n=n/10;
-			}
-			if(armstrong==original) {
-				System.out.println(original+ " is a armstrong number");
-			}else {
-				System.out.println(original+" is not armstrong number");
-			}
-		}
-
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int original = sc.nextInt();
+        int n = original;
+        int temp, armstrong = 0;
+        while (n > 0) {
+            temp = n % 10;
+            temp = (int) Math.pow(temp, 3);
+            armstrong += temp;
+            n = n / 10;
+        }
+        if (armstrong == original) {
+            System.out.println(original + " is an Armstrong number");
+        } else {
+            System.out.println(original + " is not an Armstrong number");
+        }
+    }
 }
