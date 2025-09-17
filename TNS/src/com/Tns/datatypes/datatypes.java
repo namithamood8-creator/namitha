6991bf948622905f9dea2f93fc2a061d00c0b4e4
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-09-17 18:46:25 +0530

    nami..

---
+++ TNS/src/com/Tns/datatypes/datatypes.java
@@ -0,0 +1,27 @@
+package com.Tns.datatypes;
+public class datatypes {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 3000;
+        int i = 50000;
+        long l = 15000000000L;
+        float f = 5.75f;
+        double d = 19.99;
+        char c = 'A';
+        boolean bool = true;
+        String str = "Hello Java";
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+        System.out.println("String: " + str);
+    }
+}
+
+
+
