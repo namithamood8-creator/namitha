1a5b91ef9c26851a235ed5fac9198f442eb62a5f
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-09-19 07:43:55 +0530

    Interface

---
+++ TNS/src/Abstract/AbstractDemo.java
@@ -0,0 +1,13 @@
+package Abstract;
+public class AbstractDemo {
+	public static void main(String args[]) {
+		Square s=new Square(4);
+		s.calArea();
+		s.show();
+		Rectangle r=new Rectangle(5,10);
+		r.calArea();
+		r.show();
+		
+	}
+
+}
