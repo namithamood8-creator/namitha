5672fefd4ad5fd77ec3f2fddb71047133ca74140
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-09-21 23:08:19 +0530

    array

---
+++ TNS/src/ArrayString/jagged.java
@@ -0,0 +1,21 @@
+package ArrayString;
+
+public class jagged {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][]= {
+				{1,2,3}, 
+				{4,5}, 
+				{6,7,8}
+				};
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+
