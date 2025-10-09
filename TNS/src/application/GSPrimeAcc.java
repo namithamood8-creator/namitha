8edc818cd96dbb5591139a4a13ea410e6da9ab5f
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-10-09 07:18:44 +0530

    casestudy

--- TNS/src/application/GSPrimeAcc.java
+++ TNS/src/application/GSPrimeAcc.java
@@ -1,5 +1,24 @@
 package application;
 
 public class GSPrimeAcc {
+	import com.tnsif.framework.PrimeAcc;
 
-}
+	public class GSPrimeAcc extends PrimeAcc{
+
+		public GSPrimeAcc(int accNo, String accNm, float charges, boolean isPrime) {
+			super(accNo, accNm, charges, isPrime);
+			// TODO Auto-generated constructor stub
+		}
+		public void bookProduct(float charges)
+		{
+			System.out.println("Hy "+accNm+ " your accno is : "+accNo+ " your charges are:"+charges);
+		}
+		@Override
+		public String toString() {
+			return "GSPrimeAcc [isPrime=" + isPrime + ", accNo=" + accNo + ", accNm=" + accNm + ", charges=" + charges
+					+ "]";
+		}
+		
+		
+	}
+
