5e81386782ad48d29fe57e438ed8a756d3b2069d
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-10-09 07:58:26 +0530

    jdbc

--- TNS/src/executor/JDBCMain.java
+++ TNS/src/executor/JDBCMain.java
@@ -1,5 +1,19 @@
 package executor;
 
-public class JDBCMain {
+
+
+
+	import jdbcprograms.services.CurdOperations;
+
+	public class JDBCMain {
+
+		public static void main(String[] args) {
+			CurdOperations.addStudent(101, "Seetha");
+			CurdOperations.addStudent(102, "Geetha");
+			CurdOperations.addStudent(103, "Ramu");
+
+		}
+
+	}
 
 }
