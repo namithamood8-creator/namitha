fa5be6f82291d0790ba95e60c3572cbb52dc2c01
Namitha <Namitha@DESKTOP-U0LGPN0>
2025-10-09 07:22:13 +0530

    casestudy

--- TNS/src/application/GSShopFactory.java
+++ TNS/src/application/GSShopFactory.java
@@ -1,5 +1,25 @@
 package application;
 
-public class GSShopFactory {
+	import framework.NormalAcc;
+	import framework.PrimeAcc;
+	import framework.ShopFactory;
+
+	public class GSShopFactory implements ShopFactory{
+
+		@Override
+		public PrimeAcc getNewPrimeAcc(int accNo,String accNm,float charges,boolean isPrime) {
+			PrimeAcc pri = new PrimeAcc(accNo,accNm,charges,isPrime);
+			return pri;
+		}
+
+
+		public NormalAcc getNewNormalAcc(int accNo,String accNm,float charges,float deliveryCharge) {
+			NormalAcc normal = new NormalAcc(accNo,accNm,charges,deliveryCharge);
+			return normal;
+		}
+
+		
+
+		
 
 }
